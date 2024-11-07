aae7f29315afc2b7bfddfcca740318ec5ec353be
Kaiser Sakhi <mail@kaisersakhi.com>
2024-11-07 19:48:24 +0530

    Fix issues in wallet controller

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,6 +1,7 @@
 class WalletsController < ApplicationController
   before_action :verify_request
   before_action :set_currency, except: :balances
+  before_action :set_wallet
 
   def deposit
     if params[:currency].blank? || params[:amount].blank? || params[:amount].to_i == 0
@@ -15,14 +16,14 @@ class WalletsController < ApplicationController
       return
     end
 
-    wallet = operate_on_wallet(operation: :deposit)
+    operate_on_wallet(operation: :deposit)
 
     render json: {
       status: "success",
       msg: "successfully deposited funds",
       payload: {
-        balance: wallet.balance / 100.0,
-        currency: wallet.currency.symbol
+        balance: @wallet.balance / 100.0,
+        currency: @currency.symbol
       }
     }, status: :ok
   end
@@ -34,20 +35,20 @@ class WalletsController < ApplicationController
       return
     end
 
-    if @currency.blank?
-      render json: { msg: "Currency is invalid" }, status: :not_found
+    if @currency.blank? || @wallet.blank?
+      render json: { msg: "Currency is invalid or wallet doesn't exist" }, status: :not_found
 
       return
     end
 
-    wallet = operate_on_wallet(operation: :withdraw)
+   operate_on_wallet(operation: :withdraw)
 
     render json: {
       status: "success",
       msg: "successfully withdrawn funds",
       payload: {
-        balance: wallet.balance / 100.0,
-        currency: currency.symbol
+        balance: @wallet.balance / 100.0,
+        currency: @currency.symbol
       }
     }, status: :ok
   end
@@ -70,26 +71,27 @@ class WalletsController < ApplicationController
   private
 
   def operate_on_wallet(operation:)
-    wallet = current_user.wallets.where(currency: @currency).first
     amount = params[:amount] * 100
 
-    if wallet.blank?
-      wallet = current_user.wallets.create(currency: @currency, balance: amount)
+    if @wallet.blank?
+      @wallet = current_user.wallets.create(currency: @currency, balance: amount)
     else
-      wallet.with_lock do
+      @wallet.with_lock do
         if operation == :deposit
-          wallet.balance += amount
-        else
-          wallet.balance -= amount
+          @wallet.balance += amount
+        elsif @wallet.balance >= amount
+          @wallet.balance -= amount
         end
-        wallet.save!
+        @wallet.save!
       end
     end
-
-    wallet
   end
 
   def set_currency
     @currency ||= Currency.find_by(symbol: params[:currency])
+  end
+
+  def set_wallet
+    @wallet ||= current_user.wallets.where(currency: @currency).first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   post "users/login", to: "users#login"
 
   post "/wallets/deposit", to: "wallets#deposit"
-  post "/wallet/withdrawal", to: "wallets#withdrawal"
-  get "/wallet/balances", to: "wallets#balances"
+  post "/wallets/withdrawal", to: "wallets#withdrawal"
+  get "/wallets/balances", to: "wallets#balances"
 
   post "/order/create", to: "orders#create"
   put "/order/cancel", to: "orders#cancel"
